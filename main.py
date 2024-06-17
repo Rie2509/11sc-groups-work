f954b5696704985bd9face89516fb786b67d56e2
Taiwhakaea <159851201+Taiwhakaea@users.noreply.github.com>
2024-06-18 11:19:40 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 #imports / main code.
 import random
 import time
+Score =+ 10
 QUESTION_FORMAT = "{}\na.{}\nb.{}\nc.{}\nd.{}"
-
+Play = "yes"
 #words lists
 easywords_list = ["wookie","lightspeed","lightsaber","force"]
 
@@ -29,3 +30,31 @@ if answer == d or answer == "d":
 
 #select words. 
 word = random.choice(easywords_list)
+ 
+ # check number of question attempts
+while True:
+    try:
+        tries = input("how many lives do you want")
+        tries = int(tries)
+        break
+    except:
+        print("thats not a number")
+# start the quiz
+while Play == "yes":
+
+    question_attempts = tries
+    while question_attempts > 0:
+        #ask the use a question
+
+        answer = input("are you a sigma")
+        if answer == "yes":
+            print("sigma")
+            break
+        elif answer == "":
+            print("Write something")
+        else:
+            print("no you dumb")    
+        
+        question_attempts -= 1
+        print("Try again")
+
